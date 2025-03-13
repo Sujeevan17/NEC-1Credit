95b22299be94792592a2d2c2c8ce4548e8c6e634
Sujeevan17 <sujeevan170505@gmail.com>
2025-03-13 20:16:17 +0530

    finish

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,14 @@
-Update this file to add the content
+# Use a lightweight Python base image
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy all files from the local directory to the container
+COPY . /app
+
+# Install required Python packages
+RUN pip install --no-cache-dir scikit-learn joblib
+
+# Set the default command to run the script
+CMD ["python", "ml-model.py"]
--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,25 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route("/")
+def name():
+    return "Sri Ranga Sujeevan" 
+
+
+@app.route("/registerNumber")
+def register_number():
+    return "22IT058"
+
+
+
+
+
+@app.route("/department")
+def department():
+    return "IT(Information Technology)"
+
+if __name__ == "__main__":
+    app.run(debug =True)
+    
+      
